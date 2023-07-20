1db592d86d26952545dd2b66d74f47fa3e9f6f59
Nadya Angelia <nadya.angel126@gmail.com>
2023-07-20 16:54:45 +0100

    Create navbar.blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,32 +17,15 @@
             /* Add shadow when hovered */
         }
     </style>
-
+<link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 
 <body class="bg-white">
     <!-- Navbar -->
-    <nav class="bg-white shadow">
-        <div class="container mx-auto px-4 py-2">
-            <div class="flex items-center justify-between">
-                <a href="https://flowbite.com/" class="flex items-center">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
-                    <span class="text-2xl font-semibold text-gray-800">Flowbite</span>
-                </a>
-                <button class="block md:hidden px-3 py-2 text-gray-800 hover:text-gray-900 focus:outline-none">
-                    <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
-                        <path d="M4 6h16M4 12h16m-7 6h7"></path>
-                    </svg>
-                </button>
-                <div class="hidden md:flex space-x-4">
-                    <a href="#" class="px-3 py-2 text-gray-800 hover:text-gray-900">Daftar Invoice</a>
-                    <a href="/daftarPembelian" class="px-3 py-2 text-gray-800 hover:text-gray-900">Daftar Kuitansi</a>
-
-                </div>
-            </div>
-        </div>
-    </nav>
+    @include('navbar')
+  
+   
 
     <!-- Main Content -->
     <div class="container mx-auto px-4 py-8 mt-16">
@@ -82,7 +65,7 @@
     </div>
    
 
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js"></script>
 </body>
 
 </html>
