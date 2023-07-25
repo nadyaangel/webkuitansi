c60f5ac2017df4ed7519dfad9dd35e8812548a3a
Nadya Angelia <nadya.angel126@gmail.com>
2023-07-25 04:53:04 +0100

    Layout Design Tabel Daftar Pembelian

--- resources/views/daftarpembelian.blade.php
+++ resources/views/daftarpembelian.blade.php
@@ -6,39 +6,44 @@
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
-    <table>
-        <thead>
-            <tr>
-                <th>
-                    No
-                </th>
-                <th>
-                    Nama Pembeli
-                </th>
-                <th>
-                    Tanggal
-                </th>
-                <th>
-                    Jumlah Pembelian
-                </th>
-            </tr>
-        </thead>
-        @foreach($pembelian as $item => $s)
-        <tbody>
-            <tr>
-                <th>
-                    {{($pembelian->firstItem()+$item)}}
-                </th>
-                <th>
-                    {{$s->nama}}
-                </th>
-                <td>{{$s->created_at}}</td>
-                <td>
-                    {{$s->total_harga}}
-                </td>
-            </tr>
-        </tbody>
-        @endforeach
-    </table>
+    @include('navbar')
+    <h1 class="text-lg text-center my-5 font-semibold block uppercase">Daftar Invoice</h1>
+    <div class="flex justify-center">
+        <table class="ml-20 w-3/4 my-10 text-sm text-left shadow-md">
+            <thead class="bg-blue-500 text-white">
+                <tr class="sm: px-2">
+                    <th scope="col" class="px-6 py-3">
+                        No
+                    </th>
+                    <th>
+                        Nama Pemilik Tagihan
+                    </th>
+                    <th>
+                        Tanggal
+                    </th>
+                    <th>
+                        Jumlah Pembelian
+                    </th>
+                </tr>
+            </thead>
+            @foreach($pembelian as $item => $s)
+            <tbody>
+                <tr>
+                    <td scope="row" class="px-6 py-4 font-medium">
+                        {{($pembelian->firstItem()+$item)}}
+                    </td>
+                    <td scope="row" class=" py-4 font-medium">
+                        {{$s->nama}}
+                    </td>
+                    <td scope="row" class="py-4 font-medium">{{$s->created_at}}</td>
+                    <td scope="row" class="py-4 font-medium">
+                        {{'Rp' . number_format($s->total_harga, 2, ',', '.')}}
+                    </td>
+                </tr>
+            </tbody>
+            @endforeach
+        </table>
+
+    </div>
 </body>
 </html>
