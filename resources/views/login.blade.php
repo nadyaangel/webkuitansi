670b4c5735d11a9df2e939f339e4e22094fbffdc
Nadya Angelia <nadya.angel126@gmail.com>
2023-07-26 04:08:27 +0100

    Design Login Layout finished

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -12,6 +12,20 @@
           background-repeat: no-repeat;
           background-position: center;
       }
+      input[type="password"] {
+  /* Untuk menghilangkan border */
+  border: 1px solid #ccc;
+
+  /* Atur warna border saat dalam mode fokus */
+  /* Misalnya, Anda dapat mengatur warna border saat fokus menjadi biru seperti ini: */
+  border-color: none;
+
+  /* Ganti ukuran border sesuai kebutuhan Anda */
+  border-radius: 4px;
+
+  /* Atur padding agar konten input tidak terlalu dekat dengan border */
+  padding: 0.5rem;
+}
   </style>
 </head>
 <body class="">
@@ -21,30 +35,22 @@
       <div class="bg-white rounded shadow-md px-5 py-5  md:w-1/3 mx-5 md:mx-0 mb-10 ">
       <h1 class="block uppercase font-bold text-lg text-center my-6">Login</h1>
       <p class="mx-7 text-sm text-center font-semibold my-10">Hai, masukkan username dan password yang telah didaftarkan sebelumnya ya!</p>
-      <form action="{{route('login')}}" method="post" class="w-full md:h-72 max-w-sm">
+      <form action="{{route('login')}}" method="post" class="w-full md:h-72">
         @csrf
-          <div class="md:flex md:items-center mb-6">
-            <div class="md:w-1/3">
-              <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
-                Username
-              </label>
-            </div>
-            <div class="md:w-2/3">
-              <input type="username" name="username" required class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name">
-            </div>
-          </div>
-          <div class="md:flex md:items-center mb-14">
-            <div class="md:w-1/3">
-              <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
-                Password
-              </label>
-            </div>
-            <div class="md:w-2/3">
-              <input type="password" name="password" required class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password">
-            </div>
-          </div>
-          <div class="md:flex md:items-center mb-2">
-            <div class="w-full">
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+            Username
+          </label>
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-800" id="username" type="username" name="username"  placeholder="Username">
+        </div>
+        <div class="mb-6">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+            Password
+          </label>
+          <input class="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Your Password" name="password">
+        </div>
+        <div class="md:flex md:items-center mb-2 col-md-pl-5 col-lg-pl-5">
+            <div class="w-full flex justify-center ">
               <button type="submit" class="w-full leading-tight md:ml-0 shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
                 Sign In
               </button>
